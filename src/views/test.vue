4f38afa0caa02c34050389068d26bc329150872d
kevin <621032825@qq.com>
2019-10-23 14:47:27 +0800

    1

---
+++ src/views/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <van-row type="flex" justify="center">
+      <van-col span="6" class="a">span: 6</van-col>
+      <van-col span="6" class="a">span: 6</van-col>
+      <van-col span="6" class="a">span: 6</van-col>
+    </van-row>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+<style scoped>
+.a{
+    background:red;
+}
+</style>
